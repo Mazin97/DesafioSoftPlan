7ab062042edb5fef729cefbd4d0a68d930d4a79b
mateusm <mateusm@vegait.com.br>
2022-04-27 21:03:14 -0300

    wip(show-me-the-code): adds endpoint that returns repo link

---
+++ CompoundCalcApi/Controllers/ShowMeTheCodeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace CompoundCalcApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShowMeTheCodeController : ControllerBase
+    {
+        private readonly ILogger<ShowMeTheCodeController> _logger;
+
+        public ShowMeTheCodeController(ILogger<ShowMeTheCodeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                using var client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var request = new HttpRequestMessage
+                {
+                    RequestUri = new Uri("https://api.github.com/users/mazin97/repos")
+                };
+
+                var response = await client.SendAsync(request);
+
+                var responseObject = JsonConvert.DeserializeObject<object>(await response.Content.ReadAsStringAsync());
+
+                return Ok("https://github.com/Mazin97/DesafioSoftPlan");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
